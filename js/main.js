8958048e0497f61ae9948163e0852e86f7166815
Frosaty <44754303+Frosaty@users.noreply.github.com>
2020-01-27 12:34:57 +0700

    Feature updating
    loading 50%.......
    With inner border button : http://jsfiddle.net/T9Wk7/
    and research with viewer in jquery for the project: https://fengyuanchen.github.io/jquery-viewer/

--- js/main.js
+++ js/main.js
@@ -4,3 +4,19 @@ $(document).ready(function() {
         $(this).toggleClass('open');
     });
 });
+
+// zoomIn - zoomOut
+var $image = $('#image');
+
+$image.viewer({
+    inline: true,
+    viewed: function() {
+        $image.viewer('zoomTo', 1);
+    }
+});
+
+// Get the Viewer.js instance after initialized
+var viewer = $image.data('viewer');
+
+// View a list of images
+$('#images').viewer();
